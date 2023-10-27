68f0a871e81e64170a2fc2c4af4e2509540bbfef
Santiago Lanz <Santiagolanz019@gmail.com>
2023-10-27 19:22:24 -0300

    Add Content

---
+++ js/validacion.js
@@ -0,0 +1,41 @@
+function myValidations() {
+    let password1 = document.getElementById('password1');
+    let password2 = document.getElementById('password2');
+    let terminos = document.getElementById('terminos');
+    let validity = true;
+
+
+    if (password1.value != password2.value || !password1.checkValidity()) {
+        password2.setCustomValidity(false);
+        validity = false;
+    } else {
+        password2.setCustomValidity("");
+    }
+
+
+    if (!terminos.checked) {
+        validity = false;
+        document.getElementById("btn-modal-terminos").classList.add("invalid-color");
+        document.getElementById("feedback-modal-terminos").style.display = "inline";
+    } else {
+        document.getElementById("btn-modal-terminos").classList.remove("invalid-color");
+        document.getElementById("feedback-modal-terminos").style.display = "none";
+    }
+
+    return validity;
+
+}
+
+document.getElementById("myForm").addEventListener("submit", event => {
+    if (!myValidations() || !this.checkValidity()){
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    document.body.classList.add('was-validated');
+    ['change', 'input'].forEach(ev => { document.body.addEventListener(ev, myValidations) })
+});
+
+
+
+
+
